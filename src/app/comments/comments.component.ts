0d511af283c93cfcf77b287de74385ad773217e2
juliomr21 <juliomr21.correo.com>
2022-10-05 11:43:05 -0400

    ver cambio

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -8,8 +8,8 @@ import { HttpserviceService } from '../httpservice.service';
 })
 export class CommentsComponent implements OnInit {
 
-  bd: any;
-  bb: any;
+  database: any;
+  comments: any;
   text: string = "";
   reply: string = "";
   tem: boolean = false;
@@ -17,66 +17,64 @@ export class CommentsComponent implements OnInit {
   edR: boolean = false;
   mark: boolean = false;
   com: number = 0;
-  idG: number = 10;
+  idGlobal: number = 10;
   noreply = "juliusomo"
   constructor(private httpserv: HttpserviceService) { }
 
   ngOnInit(): void {
-    this.httpserv.getJson().subscribe(res => { this.bd = res; this.bb = this.bd.comments; console.log(this.bb); });
-
+    this.httpserv.getJson().subscribe(res => { this.database = res; this.comments = this.database.comments; console.log(this.comments); });
   }
   sendReply(id: number) {
 
-
     let user = {
-
-      "id": this.idG,
+      "id": this.idGlobal,
       "content": this.reply,
-      "createdAt": "ahora",
+      "createdAt": "just now",
       "score": 0,
       "user": {
         "image": {
-          "png": this.bd.currentUser.image.png,
-          "webp": "assets/images/avatars/image-amyrobson.webp"
+          "png": this.database.currentUser.image.png,
+          "webp": ""
         },
-        "username": this.bd.currentUser.username
+        "username": this.database.currentUser.username
       },
       "replies": []
     }
-    this.idG++;
+    this.idGlobal++;
 
-    for (let i in this.bb) {
-      if (this.bb[i].id == id) {
-        this.bb[i].replies.push(user)
+    for (let i in this.comments) {
+      if (this.comments[i].id == id) {
+        this.comments[i].replies.push(user)
+        break;
       }
     }
     this.tem = false;
     this.reply = "";
-
   }
   sendRReply(id: number) {
 
 
     let user = {
 
-      "id": this.idG,
+      "id": this.idGlobal,
       "content": this.reply,
-      "createdAt": "ahora",
+      "createdAt": "Just now",
       "score": 0,
       "user": {
         "image": {
-          "png": this.bd.currentUser.image.png,
-          "webp": "assets/images/avatars/image-amyrobson.webp"
+          "png": this.database.currentUser.image.png,
+          "webp": ""
         },
-        "username": this.bd.currentUser.username
+        "username": this.database.currentUser.username
       },
       "replies": []
     }
-    this.idG++;
+    this.idGlobal++;
 
-    for (let i in this.bb) {
-      if (this.bb[i].id == id) {
-        this.bb[i].replies.push(user)
+    for (let i in this.comments) {
+      if (this.comments[i].id == id) {
+        this.comments[i].replies.push(user)
+        break;
       }
     }
     this.tem = false;
@@ -86,22 +84,22 @@ export class CommentsComponent implements OnInit {
   }
   send() {
     let user = {
-      "id": this.idG,
+      "id": this.idGlobal,
       "content": this.text,
       "createdAt": "just now",
       "score": 0,
       "user": {
         "image": {
-          "png": this.bd.currentUser.image.png,
+          "png": this.database.currentUser.image.png,
           "webp": "assets/images/avatars/image-amyrobson.webp"
         },
-        "username": this.bd.currentUser.username
+        "username": this.database.currentUser.username
       },
       "replies": []
     }
-    this.bb.push(user);
+    this.comments.push(user);
     this.text = "";
-    this.idG++;
+    this.idGlobal++;
   }
   funn(id: number, us: string) {
     this.com = id;
@@ -120,28 +118,28 @@ export class CommentsComponent implements OnInit {
 
   delet(id: number) {
     let aux = [];
-    for (let i in this.bb) {
-      if (this.bb[i].id != id)
-        aux.push(this.bb[i]);
+    for (let i in this.comments) {
+      if (this.comments[i].id != id)
+        aux.push(this.comments[i]);
     }
-    this.bb = [];
-    this.bb = aux
+    this.comments = [];
+    this.comments = aux
   }
   deleteR(idP: number, idH: number) {
     let aux = [];
-    for (let i in this.bb) {
-      if (this.bb[i].id == idP) {
+    for (let i in this.comments) {
+      if (this.comments[i].id == idP) {
 
-        for (let j in this.bb[i].replies) {
-          console.log(this.bb[i].replies[j].id);
-          if (this.bb[i].replies[j].id != idH) {
-            aux.push(this.bb[i].replies[j]);
-            console.log(this.bb[i].replies[j].id);
+        for (let j in this.comments[i].replies) {
+          console.log(this.comments[i].replies[j].id);
+          if (this.comments[i].replies[j].id != idH) {
+            aux.push(this.comments[i].replies[j]);
+            console.log(this.comments[i].replies[j].id);
           }
 
         }
-        this.bb[i].replies = [];
-        this.bb[i].replies = aux;
+        this.comments[i].replies = [];
+        this.comments[i].replies = aux;
 
       }
     }
@@ -154,10 +152,10 @@ export class CommentsComponent implements OnInit {
     }
 
     this.funn(id, us);
-    for (let i in this.bb) {
-      if (this.bb[i].id == id) {
-        this.reply = this.bb[i].content;
-        this.bb[i].content = "";
+    for (let i in this.comments) {
+      if (this.comments[i].id == id) {
+        this.reply = this.comments[i].content;
+        this.comments[i].content = "";
         this.ed = false;
         break;
       }
@@ -166,11 +164,11 @@ export class CommentsComponent implements OnInit {
   }
   editR(idP: number, idH: number) {
     this.f(idH);
-    for (let i in this.bb) {
-      if (this.bb[i].id == idP) {
-        for (let j in this.bb[i].replies) {
-          if (this.bb[i].replies[j].id == idH) {
-            this.reply = this.bb[i].replies[j].content;
+    for (let i in this.comments) {
+      if (this.comments[i].id == idP) {
+        for (let j in this.comments[i].replies) {
+          if (this.comments[i].replies[j].id == idH) {
+            this.reply = this.comments[i].replies[j].content;
             break;
           }
         }
@@ -180,9 +178,9 @@ export class CommentsComponent implements OnInit {
 
   }
   updateText(id: number) {
-    for (let i in this.bb) {
-      if (this.bb[i].id == id) {
-        this.bb[i].content = this.reply;
+    for (let i in this.comments) {
+      if (this.comments[i].id == id) {
+        this.comments[i].content = this.reply;
         break;
       }
 
@@ -192,13 +190,13 @@ export class CommentsComponent implements OnInit {
     this.ed = true;
   }
   updateTextR(idP: number, idH: number) {
-    for (let i in this.bb) {
-      if (this.bb[i].id == idP) {
-        for(let j in this.bb[i].replies)
+    for (let i in this.comments) {
+      if (this.comments[i].id == idP) {
+        for(let j in this.comments[i].replies)
         {
-          if(this.bb[i].replies[j].id == idH)
+          if(this.comments[i].replies[j].id == idH)
           {
-            this.bb[i].replies[j].content = this.reply;
+            this.comments[i].replies[j].content = this.reply;
             break;
           }
         }
